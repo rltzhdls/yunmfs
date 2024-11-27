36a5d6c74d6dc6aecd5e881c618f4fbd1a28d28c
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-11-27 16:30:58 +0900

    Add files via upload

---
+++ yunmfs.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/lineage/2024/1030/v2/intro.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
